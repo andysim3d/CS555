3a9855f33874f04dd705b9deeab68841a9abd951
gao87926 <gao87926@gmail.com>
2014-02-24 23:03:28 -0500

    Finished structure of Input convert to node

---
+++ cs555/src/edu/stevens/cs555/factory/Input2Node.java
@@ -0,0 +1,43 @@
+/* Name: Input2Node.java
+ * Author: Gao Xing
+ * Date: 
+ * 
+ * Function: getInstance than use get
+ */
+package edu.stevens.cs555.factory;
+import java.util.Hashtable;
+
+import edu.stevens.cs555.entities.*;
+public class Input2Node {
+	
+	private static Input2Node instance =null;
+	private Input2Node() {
+				
+	}
+	
+	public Input2Node getInstance(){
+		if(instance==null){
+			instance = new Input2Node();
+		}
+	  return instance;
+	}
+	
+	private static Hashtable<String, Individual> indNode=null;
+	private static Hashtable<String, Family> fmNode=null;
+	
+	 public static void convertNode(String path){
+		 
+		 
+		 
+		 
+	 }
+	 
+	 public Hashtable<String, Individual> getIndNode(){
+		 return this.indNode;
+	 }
+	 public Hashtable<String, Family> getfmNode(){
+		 return this.fmNode;
+	 }
+
+
+}
