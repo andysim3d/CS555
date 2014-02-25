b5e8a8e967953739bee9e73205939d7c8e278259
Pengfei Zhang <pzhang11@stevens.edu>
2014-02-24 23:09:50 -0500

    Test Display Error
    Signed-off-by: Pengfei Zhang <pzhang11@stevens.edu>

---
+++ cs555/src/edu/stevens/cs555/testModel/TestForDisplayError.java
@@ -0,0 +1,51 @@
+/* Name: TestForDisplayError.java
+ * Author: Pengfei Zhang
+ * Date: 24/02/2014
+ * 
+ * Function: Test DisplayErrors method
+ */
+
+package edu.stevens.cs555.testModel;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.stevens.cs555.*;
+import edu.stevens.cs555.entities.Individual;
+
+public class TestForDisplayError {
+	
+
+	private Individual indi1;
+	private Individual indi2;
+	
+	@BeforeClass
+	public static void testSetup(){
+		
+	}
+	@AfterClass
+	public static void testCleanup(){
+		
+	}
+	
+	/*@Test(expected = java.io.FileNotFoundException.class)
+	public void testExcep() throws IOException{
+		Proj1 target = new Proj1();
+		target.read("//Users//andy//Documents//workspace//cs555//bin//GU1.txt");
+	}*/
+	
+	@Test
+	public void test() throws IOException {
+		indi
+		Proj1 target = new Proj1();
+		
+		//assertEquals("", 1, );
+	}
+	
+	
+
+}
