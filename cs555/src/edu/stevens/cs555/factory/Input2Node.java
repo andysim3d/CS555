7346dd33efc3c2858d39360c6781fafc22b300b0
gao87926 <gao87926@gmail.com>
2014-02-24 23:05:57 -0500

    change

--- cs555/src/edu/stevens/cs555/factory/Input2Node.java
+++ cs555/src/edu/stevens/cs555/factory/Input2Node.java
@@ -5,39 +5,39 @@
  * Function: getInstance than use get
  */
 package edu.stevens.cs555.factory;
+
 import java.util.Hashtable;
 
 import edu.stevens.cs555.entities.*;
+
 public class Input2Node {
-	
-	private static Input2Node instance =null;
+
+	private static Input2Node instance = null;
+
 	private Input2Node() {
-				
+
 	}
-	
-	public Input2Node getInstance(){
-		if(instance==null){
+
+	public Input2Node getInstance() {
+		if (instance == null) {
 			instance = new Input2Node();
 		}
-	  return instance;
+		return instance;
 	}
-	
-	private static Hashtable<String, Individual> indNode=null;
-	private static Hashtable<String, Family> fmNode=null;
-	
-	 public static void convertNode(String path){
-		 
-		 
-		 
-		 
-	 }
-	 
-	 public Hashtable<String, Individual> getIndNode(){
-		 return this.indNode;
-	 }
-	 public Hashtable<String, Family> getfmNode(){
-		 return this.fmNode;
-	 }
 
+	private Hashtable<String, Individual> indNode = null;
+	private Hashtable<String, Family> fmNode = null;
+
+	public static void convertNode(String path) {
+
+	}
+
+	public Hashtable<String, Individual> getIndNode() {
+		return this.indNode;
+	}
+
+	public Hashtable<String, Family> getfmNode() {
+		return this.fmNode;
+	}
 
 }
