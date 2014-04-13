4d161925440c4ac1642da3f526ecf295c4f43642
Pengfei Zhang <pzhang11@stevens.edu>
2014-04-13 18:23:36 -0400

    implement CheckBigamy.java
    Signed-off-by: Pengfei Zhang <pzhang11@stevens.edu>

--- cs555/src/edu/stevens/cs555/validation/CheckBigamy.java
+++ cs555/src/edu/stevens/cs555/validation/CheckBigamy.java
@@ -9,63 +9,59 @@ import edu.stevens.cs555.entities.Family;
 import edu.stevens.cs555.entities.Individual;
 
 public class CheckBigamy {
-	
-	public static ArrayList<ErrorOut> MarryCheck(Hashtable<String, Family> fmNode, Hashtable<String, Individual> indNode)
-	{
-	
+
+	public static ArrayList<ErrorOut> MarryCheck(
+			Hashtable<String, Family> fmNode,
+			Hashtable<String, Individual> indNode) {
+
 		ArrayList<ErrorOut> errors = new ArrayList<ErrorOut>();
-		
-		for (String key: indNode.keySet()) {
+
+		for (String key : indNode.keySet()) {
 			ArrayList<String> fcc = new ArrayList<String>();
-			//scan all families and find whose wife or husband is key
-			for(String fmk : fmNode.keySet()){
-				if ((fmNode.get(fmk).getHusb() == key )||(fmNode.get(fmk).getWife() == key )) {
+			// scan all families and find whose wife or husband is key
+			for (String fmk : fmNode.keySet()) {
+				if ((fmNode.get(fmk).getHusb() == key)
+						|| (fmNode.get(fmk).getWife() == key)
+						&& ((fmNode.get(fmk).isMarr()) && (fmNode.get(fmk)
+								.isDiv()))) {
 					fcc.add(fmk);
 				}
-			//compare all marry date and div date
-			
-			
-				
-				
-			}
-			
-			
-			
-			
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		for(String key : fmNode.keySet()){
-			//if No marry date exist, continue
-			if((fmNode.get(key).isMarr())){
-				//else, save marry date
-				Date marry = fmNode.get(key).getMarr_date();
-				//if husband is dead, check his death date and compare
-				if(indNode.get(fmNode.get(key).getHusb()).isDead()){
-					if(indNode.get(fmNode.get(key).getHusb()).getDeathday().before(marry)){
-						errors.add(new ErrorOut("4",true , "Inividual(" +fmNode.get(key).getWife() + ")" + "marries to a dead people("
-								+ fmNode.get(key).getHusb()+")"));
+				// compare all marry date and div date
+				if (fcc.size() > 1) {
+					for (int i = 0; i < fcc.size() - 1; i++) {
+						for (int j = i; j < fcc.size(); j++) {
+							if (InterVal(fmNode.get(fcc.get(i)),
+									fmNode.get(fcc.get(j)))) {
+								errors.add(new ErrorOut("10",true ,"Individal" + key+ "is a bigamy"));
+							}
+						}
 					}
+
 				}
-				if(indNode.get(fmNode.get(key).getWife()).isDead()){
-					if(indNode.get(fmNode.get(key).getWife()).getDeathday().before(marry)){
-						errors.add(new ErrorOut("4",true , "Inividual(" +fmNode.get(key).getHusb() + ")" + "marries to a dead people("
-								+ fmNode.get(key).getWife() + ")"));
-					}
+				// else, he/she must can't be bigamy.
+				else {
+					continue;
 				}
-				
-				
+
 			}
-			
+
 		}
 		return errors;
 	}
+
+	public static Boolean InterVal(Family fm1, Family fm2) {
+		if (fm1.getMarr_date_aval() && fm1.getDiv_date_aval()
+				&& fm2.getDiv_date_aval() && fm2.getMarr_date_aval()) {
+
+			if ((fm1.getMarr_date().before(fm2.getMarr_date()) && (fm1
+					.getDiv_date().after(fm2.getMarr_date())))) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
 }
