214310d241118b1fa5d368faf1a72f9e77444637
ruixiangchu <Ray@localhost>
2014-04-15 11:57:42 -0400

    Upload CheckMBB.java and CheckMTS.java
    Signed-off-by: ruixiangchu <Ray@localhost>

---
+++ cs555/src/edu/stevens/cs555/validation/CheckMBB.java
@@ -0,0 +1,32 @@
+/* Name: CheckMBB.java
+ * Author: Ruixiang Chu
+ * Date: 07/04/2014
+ * 
+ * Function: Check if a person marry to himself
+ */
+package edu.stevens.cs555.validation;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import edu.stevens.cs555.entities.ErrorOut;
+import edu.stevens.cs555.entities.Family;
+import edu.stevens.cs555.entities.Individual;
+
+public class CheckMBB {
+	public static ArrayList<ErrorOut> mbbCheck(Hashtable<String, Family> fmNode, Hashtable<String, Individual> indNode){
+		ArrayList<ErrorOut> errors = new ArrayList<ErrorOut>();
+		for(String key : fmNode.keySet()){
+			Family family = fmNode.get(key);
+			Individual father = indNode.get(family.getHusb());
+			Individual mother = indNode.get(family.getWife());
+			
+			if(family.isMarr() && family.getMarr_date_aval() && father.isBorn() && mother.isBorn()){
+				if(family.getMarr_date().before(father.getBirthday()) || family.getMarr_date().before(mother.getBirthday())){
+					errors.add(new ErrorOut("3", true , "Family(" +key + ")" + "'s husband and wife are married before their birth"));
+				}
+			}
+		}
+		return errors;
+	}
+}
---
+++ cs555/src/edu/stevens/cs555/validation/CheckMTS.java
@@ -0,0 +1,36 @@
+/* Name: CheckMTS.java
+ * Author: Ruixiang Chu
+ * Date: 07/04/2014
+ * 
+ * Function: Check if a person marry to himself
+ */
+package edu.stevens.cs555.validation;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import edu.stevens.cs555.entities.ErrorOut;
+import edu.stevens.cs555.entities.Family;
+
+public class CheckMTS {
+	public static ArrayList<ErrorOut> mtsCheck(Hashtable<String, Family> fmNode){
+		ArrayList<ErrorOut> errors = new ArrayList<ErrorOut>();
+		for(String key : fmNode.keySet()){
+			Family family = fmNode.get(key);
+			String father = family.getHusb();
+			String mother = family.getWife();
+			
+			if(father != null && mother != null && !father.equals(mother)){
+				for(String familyKey : fmNode.keySet()){
+					Family innerFamily = fmNode.get(familyKey);
+					ArrayList<String> children = innerFamily.getChil();
+					
+					if(children.contains(father) && children.contains(mother)){
+						errors.add(new ErrorOut("3", true , "Family(" +key + ")" + "'s husband and wife are sablings"));
+					}
+				}
+			}
+		}
+		return errors;
+	}
+}
